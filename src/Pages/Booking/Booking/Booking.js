e438ffd9474e1e82c1279ce88b70fe6ec9b92352
rezwan <rezwan6@gmail.com>
2021-10-31 18:44:31 +0600

    styled booking page. looks ready

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
+import useAuth from '../../../hooks/useAuth';
+import './Booking.css';
 
 const Booking = () => {
     const { serviceId } = useParams();
     const [service, setService] = useState({})
+
+    const { register, formState: { errors } } = useForm();
+    const { user } = useAuth();
 
     useEffect(() => {
         fetch(`https://quiet-coast-20979.herokuapp.com/services/${serviceId}`)
@@ -13,13 +19,32 @@ const Booking = () => {
 
     return (
         <div className="mt-5 pt-5">
-            <h2>Details of: {service.destination}</h2>
-            <img src={service.img} alt="" />
-            <h2>Booking Trip: {serviceId}</h2>
-            <p>4. Clicking on the button will take the user to the place order page. This route will be a private/protected route. Please make sure that if the user is not logged in, the private route redirects to the login page. After login, the user will be redirected to the page he/she wanted to go to. Also, make sure, after reloading the page of a private route, the user is not redirected to the login page.</p>
-            <p>
-                5. The Place Order page should display detailed information of the item, user has clicked. It will display users name and email address from the login system and user will provide address and necessary information to place the order/booking/purchase.
-            </p>
+            <div className="booking-top mt-2">
+                <h2 className="mb-4">{service.destination}</h2>
+                <img src={service.img} className="booking-img mb-4" alt="" />
+                <p>{service.bestFor}</p>
+                <h6>Tour Duration: {service.duration} day(s)</h6>
+                <h5 className="mt-4">Price: {service.cost} /person</h5>
+            </div>
+            <div className="my-5">
+                <h3 className="mb-4">Book a trip to {service.destination}</h3>
+                <form className="booking-form">
+                    <label>Name</label>
+                    <input defaultValue={user.displayName} {...register("name")} />
+                    <label>Email</label>
+                    <input defaultValue={user.email} {...register("email", { required: true })} />
+                    {errors.email && <span className="error">This field is required</span>}
+                    <label>Country</label>
+                    <input placeholder="Country" defaultValue="" {...register("city")} />
+                    <label>City</label>
+                    <input placeholder="City" defaultValue="" {...register("city")} />
+                    <label>Street Address</label>
+                    <input placeholder="Street Address" defaultValue="" {...register("address")} />
+                    <label>Phone Number</label>
+                    <input placeholder="phone number" defaultValue="" {...register("phone")} />
+                    <input className="btn btn-warning mt-3 w-50 mx-auto" type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
